481f342e86e071cc0fbf7989deb4c93a6986bc26
gulshan-09 <152474356+gulshan-09@users.noreply.github.com>
2024-10-30 02:16:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const chrome = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
-const express = require('express');
+import express from 'express';
+import puppeteer from 'puppeteer-core';
+import chrome from 'chrome-aws-lambda';
+
 const app = express();
 
 app.get('/fetch-html', async (req, res) => {
     let browser = null;
     try {
-        // Puppeteer ko lambda-compatible launch options ke sath configure karein
         browser = await puppeteer.launch({
             args: chrome.args,
             executablePath: await chrome.executablePath,
@@ -17,10 +17,8 @@ app.get('/fetch-html', async (req, res) => {
             waitUntil: 'networkidle2'
         });
 
-        // Wait for the content to load
         await new Promise(resolve => setTimeout(resolve, 5000));
         const content = await page.content();
-
         await browser.close();
 
         res.json({ content });
@@ -30,4 +28,4 @@ app.get('/fetch-html', async (req, res) => {
     }
 });
 
-module.exports = app;
+export default app;
