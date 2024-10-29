57d2c8879a67883dd95296f019e4e943b0af1b25
gulshan-09 <152474356+gulshan-09@users.noreply.github.com>
2024-10-30 05:29:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.use(cors());
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
+app.use((req, res, next) => {
+    // CORS headers
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 // Endpoint to handle the proxy request
 app.post('/proxy-download', async (req, res) => {
     const { captcha_v3, id } = req.body;
